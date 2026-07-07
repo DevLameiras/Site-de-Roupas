3348953a25188407b0db7c1fbb3080d0b0883841
DevLameiras <umgansoqualquer@gmail.com>
2026-07-07 11:14:31 -0300

    Criando a section de Mais Vendidas, usando swiper para criar carrossel com efeito 3D.

--- src/js/carrossel.js
+++ src/js/carrossel.js
@@ -21,4 +21,44 @@ function proximoCard(){
 
 mostrarCard(cardAtual);
 
-setInterval(proximoCard, 4000)
+setInterval(proximoCard, 4000);
+
+
+
+// Carrossel Mais Vendidas
+
+
+var swiper = new Swiper(document.querySelector(".swiper"), {
+    effect:"coverflow",
+    grabCursor:true,
+    centeredSlides:true,
+    coverflowEffect:{
+        rotate:0,
+        stretch:0,
+        depth:100,
+        modifier:4,
+        slideShadows:true
+    },
+    loop:true,
+    navigation:{
+        nextEl:".swiper-button-next",
+        prevEl:".swiper-button-prev"
+    },
+    keyboard:{
+        enabled:true
+    },
+    mousewheel:{
+        thresholdDelta:70
+    },
+    breakpoints:{
+        560:{
+            slidesPerView:2.5
+        },
+        768:{
+            slidesPerView:3
+        },
+        1024:{
+            slidesPerView: 3
+        }
+    }
+});
